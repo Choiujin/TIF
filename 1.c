878329d9e6ebd61ad3c719738ee67cdbfae383b7
Choiujin <choiujin0725@gmail.com>
2024-09-20 16:41:38 +0900

    1번 문제 수정

--- 1.c
+++ 1.c
@@ -5,6 +5,7 @@ int main()
 	int num1[]={1,2,3}, num2[]={4,5,6}, N1,N2,N;
 	int target = 8;
 	
+	
 	printf("먼저 num1에서 1~3중 몇 번째 숫자를 더할까요?\n");
 	scanf("%d",&N1);
 	printf("\n num2에서 1~3중 몇 번째 숫자를 더할까요?\n");
